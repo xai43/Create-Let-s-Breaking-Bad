fd98a4fc23eb57d18e677aa16433e78b7fc143a0
Takhr <khai.44.13.572@gmail.com>
2026-05-05 18:54:14 +0300

    ~~cyanide and test_radish~~ datagen fix

--- src/main/java/net/khai/create_lets_breaking_bad/Create_Lets_Breaking_Bad.java
+++ src/main/java/net/khai/create_lets_breaking_bad/Create_Lets_Breaking_Bad.java
@@ -3,9 +3,11 @@ package net.khai.create_lets_breaking_bad;
 import net.khai.create_lets_breaking_bad.block.ModBlocks;
 import net.khai.create_lets_breaking_bad.item.ModCreativeModTabs;
 import net.khai.create_lets_breaking_bad.item.ModItems;
+import net.minecraft.data.DataGenerator;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
@@ -20,7 +22,6 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 @Mod(Create_Lets_Breaking_Bad.MOD_ID)
 public class Create_Lets_Breaking_Bad {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "create_lets_breaking_bad";
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -40,6 +41,12 @@ public class Create_Lets_Breaking_Bad {
 
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+    }
+
 
 
     private void commonSetup(FMLCommonSetupEvent event) {
